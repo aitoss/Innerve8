a9a73f1a86471d4b070215ac5771e51cb72d5620
404Nikhil <nicksd111@gmail.com>
2023-12-05 09:11:44 +0530

    deploy fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import Nav from './Components/Nav/nav.jsx'
-import Home from './pages/home.jsx'
+import Home from './Pages/home.jsx'
 import Prize from './Pages/prize.jsx'
 function App() {
   const [count, setCount] = useState(0)
