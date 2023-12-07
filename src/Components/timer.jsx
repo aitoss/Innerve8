51c8a70c9aa5e023ed9149c9e8369ebfb6590ed7
Gourav2609 <herculeswarrior.in@gmail.com>
2023-12-08 04:19:29 +0530

    Animation

--- src/Components/timer.jsx
+++ src/Components/timer.jsx
@@ -42,29 +42,29 @@ export default function Timer() {
   const { days, hours, minutes, seconds } = timeRemaining;
 
   return (
-    <div className="flex flex-row w-4/6 h-full justify-between pt-14 max-w-5xl">
+    <div className="flex flex-row w-3/6 h-full justify-between pt-14 max-w-3xl">
       <div className="">
-        <span className="flex flex-col w-24 h-24 justify-center border-2 border-dayColor rounded-md ">
-          <p className="text-black flex justify-center text-lg md:text-3xl lg:text-4xl xl:text-5xl">{days}</p>
-          <p className="text-black flex justify-center">Days</p>
+        <span className="flex flex-col w-24 h-24 justify-center border-2 bg-dayColor rounded-md">
+          <p className="text-white flex justify-center text-lg md:text-3xl lg:text-4xl xl:text-5xl">{days}</p>
+          <p className="text-white flex justify-center">Days</p>
         </span>
       </div>
       <div>
-        <span className="flex flex-col  w-[100px] h-[100px] justify-center border-2 border-hourColor rounded-md">
-          <p className="text-WHITE flex justify-center text-lg md:text-3xl lg:text-4xl xl:text-5xl">{hours}</p>
+        <span className="flex flex-col  w-24 h-24 justify-center border-2 bg-hourColor rounded-md">
+          <p className="text-white flex justify-center text-lg md:text-3xl lg:text-4xl xl:text-5xl">{hours}</p>
           <p className="text-white flex justify-center">Hours</p>
         </span>
       </div>
       <div>
-        <span className="flex flex-col  w-24 h-24 justify-center border-2 border-minuteColor rounded-md">
+        <span className="flex flex-col  w-24 h-24 justify-center border-2 bg-minuteColor rounded-md">
           <p className="text-white flex justify-center text-lg md:text-3xl lg:text-4xl xl:text-5xl">{minutes}</p>
           <p className="text-white flex justify-center">Minutes</p>
         </span>
       </div>
       <div>
-        <span className="flex flex-col  w-24 h-24 justify-center border-2 border-secondColor rounded-md">
-          <p className="text-black flex justify-center text-lg md:text-3xl lg:text-4xl xl:text-5xl">{seconds}</p>
-          <p className="text-black flex justify-center">Seconds</p>
+        <span className="flex flex-col  w-24 h-24 justify-center border-2 bg-secondColor rounded-md">
+          <p className="text-white flex justify-center text-lg md:text-3xl lg:text-4xl xl:text-5xl">{seconds}</p>
+          <p className="text-white flex justify-center">Seconds</p>
         </span>
       </div>
     </div>
