6874c7f4e28b9179c63c66c3dea229ff59a6bd0a
Lokendra Kushwah <118094744+Lokendrakushwah12@users.noreply.github.com>
2023-12-07 22:11:28 +0530

    grad bg in timer

--- src/Components/timer.jsx
+++ src/Components/timer.jsx
@@ -42,29 +42,29 @@ export default function Timer() {
   const { days, hours, minutes, seconds } = timeRemaining;
 
   return (
-    <div className="flex flex-row w-4/6 h-full justify-between pt-14 max-w-5xl">
+    <div className="flex flex-row w-5/12 h-full justify-between gap-4 pt-14 max-w-5xl">
       <div className="">
-        <span className="flex flex-col w-24 h-24 justify-center border-2 border-dayColor rounded-md ">
-          <p className="text-black flex justify-center text-lg md:text-3xl lg:text-4xl xl:text-5xl">{days}</p>
-          <p className="text-black flex justify-center">Days</p>
+        <span className="flex flex-col w-[100px] h-[100px] justify-center bg-gradient-to-t from-[#DF6E47] via-[#E7885A] to-[#F0A36D] rounded-xl ">
+          <p className="text-[hsla(0,0%,100%,90%)] flex justify-center text-lg md:text-3xl lg:text-4xl xl:text-5xl">{days}</p>
+          <p className="text-[hsla(0,0%,100%,90%)] flex justify-center">Days</p>
         </span>
       </div>
       <div>
-        <span className="flex flex-col  w-[100px] h-[100px] justify-center border-2 border-hourColor rounded-md">
-          <p className="text-WHITE flex justify-center text-lg md:text-3xl lg:text-4xl xl:text-5xl">{hours}</p>
-          <p className="text-white flex justify-center">Hours</p>
+        <span className="flex flex-col  w-[100px] h-[100px] justify-center bg-gradient-to-t from-[#8FB781] via-[#90C280] to-[#C1D29D] rounded-xl">
+          <p className=" flex justify-center text-lg md:text-3xl lg:text-4xl xl:text-5xl">{hours}</p>
+          <p className=" flex justify-center">Hours</p>
         </span>
       </div>
       <div>
-        <span className="flex flex-col  w-24 h-24 justify-center border-2 border-minuteColor rounded-md">
-          <p className="text-white flex justify-center text-lg md:text-3xl lg:text-4xl xl:text-5xl">{minutes}</p>
-          <p className="text-white flex justify-center">Minutes</p>
+        <span className="flex flex-col  w-[100px] h-[100px] justify-center bg-gradient-to-t from-[#ca80b1] via-[#F27CCA] to-[#f5aedd] rounded-xl">
+          <p className=" flex justify-center text-lg md:text-3xl lg:text-4xl xl:text-5xl">{minutes}</p>
+          <p className=" flex justify-center">Minutes</p>
         </span>
       </div>
       <div>
-        <span className="flex flex-col  w-24 h-24 justify-center border-2 border-secondColor rounded-md">
-          <p className="text-black flex justify-center text-lg md:text-3xl lg:text-4xl xl:text-5xl">{seconds}</p>
-          <p className="text-black flex justify-center">Seconds</p>
+        <span className="flex flex-col  w-[100px] h-[100px] justify-center bg-gradient-to-t from-[#FDDE56] via-[#FFEA8C] to-[#FDF0B7] rounded-xl">
+          <p className="text-[hsla(0,0%,100%,90%)] flex justify-center text-lg md:text-3xl lg:text-4xl xl:text-5xl">{seconds}</p>
+          <p className="text-[hsla(0,0%,100%,90%)] flex justify-center">Seconds</p>
         </span>
       </div>
     </div>
