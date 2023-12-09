3e891eea2cdada6385229ebde8f42c473150c5aa
404Nikhil <nicksd111@gmail.com>
2023-12-10 00:23:03 +0530

    alignment fix

--- src/Pages/bannercard.jsx
+++ src/Pages/bannercard.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
+import { IoMdMail } from "react-icons/io";
 
 const Bannercard = () => {
   return (
     <>
-    <section className="bg-[#121212] w-screen p-16 h-[60vh]">
-        <div className="bg-green-300 flex flex-row rounded-3xl p-16 h-[45vh] shadow-xl ">
-        <div className="flex space-y-32 flex-col">
-        <p className="text-[#121212] text-[48px] font-medium">Got any question for us?</p>
-        <p className="text-[#121212] text-[48px] font-medium">Want to sponsor us</p>
-        </div>
-        <div className="flex flex-col">
+ <section className="bg-[#121212] w-screen p-16 h-[60vh]">
+            <div className="bg-green-300 flex flex-row rounded-3xl p-16 h-[45vh] shadow-xl">
+                <div className="flex flex-col w-[50%] space-y-8">
+                    <p className="text-[#121212] text-[48px] font-medium">Got any question for us?</p>
+                    <p className="text-[#121212] text-[48px] font-medium">Want to sponsor us?</p>
+                </div>
 
-        </div>
-        </div>
-
-    </section>
+                <div className="flex flex-col w-[50%] items-end justify-center">
+                    <div className="flex text-3xl text-black gap-4 items-center">
+                        <IoMdMail className="text-4xl" />
+                        <p>@aitpune.edu.in</p>
+                    </div>
+                </div>
+            </div>
+        </section>
     </>
   )
 }
