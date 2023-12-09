636e3c937379903d980c173eabbd28dded670821
Lokendra Kushwah <118094744+Lokendrakushwah12@users.noreply.github.com>
2023-12-09 16:52:56 +0530

    innerve wave

--- src/Pages/threejs.jsx
+++ src/Pages/threejs.jsx
@@ -9,8 +9,8 @@ export default function Threejs() {
       <Canvas>
         {/* <OrbitControls  enableZoom={false} /> */}
         {/* <ambientLight intensity={10} /> */}
-        <directionalLight position={[2, 2, 8.407]} intensity={3} />
-        <group position={[14 , -7.5 , -13]}>
+        <directionalLight position={[1, 1, 6]} intensity={4} />
+        <group position={[14 , -7 , -14]}>
           <mesh>
             {/* <LaptopModel scale={{ scale: 1, position: 0.5 }} /> */}
             <Infinity scale={{ scale: 3, position: 0.5 }} />
