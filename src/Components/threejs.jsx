11140f306a9c8b86228c2f625feb495ca3eaf5d8
Harshal patil <harshalmukundapatil@gmail.com>
2023-12-11 23:34:10 +0530

    relocating

--- src/Components/threejs.jsx
+++ src/Components/threejs.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
-import Infinity from "../../infiniteLoop";
+import Infinity from "./infiniteLoop";
 import Controls from "./control";
 
 export default function Threejs() {
