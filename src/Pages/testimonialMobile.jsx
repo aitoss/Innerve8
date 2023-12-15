9486ce285040b89f4ccd31f72ed17e696bf5df16
404Nikhil <nicksd111@gmail.com>
2023-12-15 21:54:48 +0530

    testi mob

--- src/Pages/testimonialMobile.jsx
+++ src/Pages/testimonialMobile.jsx
@@ -14,10 +14,12 @@ const TestimonialMob = () => {
     const y = useTransform(scrollYProgress, [0, 1], [1, -150]);
     const y2 = useTransform(scrollYProgress, [0, 1], [1, 150]);
     const images = [
-        "1.jpg",
-        "2.jpg",
-        "3.jpg",
-        "4.jpg",
+        "1.JPG",
+        "2.JPG",
+        "3.JPG",
+        "4.JPG",
+        "5.JPG",
+        "6.JPG",
     ];
 
     useEffect(() => {
@@ -42,7 +44,7 @@ const TestimonialMob = () => {
                     style={{ y }}
                 >
                     <Column className=" w-1/3 h-[40%] relative flex -top-[10%] flex-col gap-[2vw] " images={[images[0], images[1], images[2]]} y={y} />
-                    <Column className=" h-[40%] w-1/3 flex -top-[75%] flex-col gap-[2vw] " images={[images[1], images[0], images[2]]} y={y2} />
+                    <Column className=" h-[40%] w-1/3 flex -top-[75%] flex-col gap-[2vw] " images={[images[3], images[4], images[5]]} y={y2} />
                    
                 </div>
             </section>
@@ -61,7 +63,7 @@ const Column = ({ images, y = 0 }) => {
                     return (
                         <div key={index} className="w-[100%] py-2 rounded-lg overflow-hidden relative  ">
                             <img className="object-cover"
-                                src={`/images/${src}`} fill
+                                src={`/images/testi/${src}`} fill
                                 alt='image'
                             />
                         </div>
