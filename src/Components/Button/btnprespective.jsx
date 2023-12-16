f855b89845f03bd4e93706e72dd475aedd71d1be
Lokendra Kushwah <118094744+Lokendrakushwah12@users.noreply.github.com>
2023-12-16 15:38:04 +0530

    created btn component

---
+++ src/Components/Button/btnprespective.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './btn.css'
+
+const btnprespective = ({label}) => {
+    return (
+        <div className="press w-full h-full flex flex-col justify-center items-center">
+            <p className="press__inner1">{label}</p>
+            <p className="press__inner2">{label}</p>
+        </div>
+    )
+}
+
+export default btnprespective
