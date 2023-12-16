9d052ccf0740edd1e34ff97dae37b201703861f2
404Nikhil <nicksd111@gmail.com>
2023-12-16 21:31:14 +0530

    added gotop

--- src/Components/GoTop.jsx
+++ src/Components/GoTop.jsx
@@ -9,7 +9,7 @@ const GoToTop = () => {
   };
 
   const listenToScroll = () => {
-    let heightToHidden = 20;
+    let heightToHidden = 10;
     const winScroll = window.scrollY || document.documentElement.scrollTop;
 
     if (winScroll > heightToHidden) {
@@ -28,7 +28,7 @@ const GoToTop = () => {
     <div className="flex justify-center items-center relative">
     {isVisible && (
       <div
-        className="top-btn fixed bottom-5 right-5 z-[99999999] flex justify-center items-center cursor-pointer p-4 m-4 bg-[#121212] border-1 rounded-full"
+        className="top-btn fixed bottom-2 right-2 z-[99999999] flex justify-center items-center cursor-pointer p-3 m-3 bg-[#121212] border-1 rounded-full"
         onClick={goToBtn}
       >
         <FaArrowUp className="top-btn--icon animate-gototop " />
