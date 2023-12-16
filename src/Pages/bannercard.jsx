84af67ebb76306d8f1065aa83852b38cb9659089
Gourav2609 <herculeswarrior.in@gmail.com>
2023-12-16 19:37:00 +0530

    Added mail

--- src/Pages/bannercard.jsx
+++ src/Pages/bannercard.jsx
@@ -4,14 +4,19 @@ import { IoLogoInstagram } from "react-icons/io";
 import { IoIosArrowRoundForward } from "react-icons/io";
 
 const Bannercard = () => {
+
+    const handleEmailClick = () => {
+        window.location.href = 'mailto:aitoss@aitpune.edu.in';
+    };
+
     return (
         <>
             <section className=" md:px-10 mx-8 mb-8">
                 <div className="banner flex flex-col gap-8 p-12 max-w-screen-xl" style={{margin: 'auto'}}>
                     <div className="banner-sub w-full flex justify-between items-center">
                         <p className="banner-heading font-medium">Got questions? <br />Contact us</p>
-                        <div className="btn group flex items-center justify-center gap-3 border border-[#212121] rounded-full p-2 pl-5 cursor-pointer">
-                            <a className="banner-subheading font-[400] text-[#212121] hover:text-[#212121] hover:cursor-pointer transition-all" href=''>aitoss@aitpune.edu.in</a>
+                        <div className="btn group flex items-center justify-center gap-3 border border-[#212121] rounded-full p-2 pl-5 cursor-pointer" onClick={handleEmailClick}>
+                            <a className="banner-subheading font-[400] text-[#212121] hover:text-[#212121] hover:cursor-pointer transition-all -z-1" >aitoss@aitpune.edu.in</a>
                             <div className="w-[30px] h-[30px] flex items-center justify-center text-base text-[#121212e6] border border-[#121212e6] rounded-full group-hover:rotate-45  transition-all">↗</div>
                         </div>
                     </div>
