defd6cc45b013ba9aa2ae2c326a2b85000c0ffbb
404Nikhil <nicksd111@gmail.com>
2024-01-20 14:26:23 +0530

    mobilenav fix

--- src/Components/Nav/mobileNav.jsx
+++ src/Components/Nav/mobileNav.jsx
@@ -30,7 +30,7 @@ const mobileNav = ({ title }) => {
     return (
         <motion.div
             variants={mobileNavLinks}
-            className="overflow-hidden ml-10 w-[70%] sm:w-[80%] text-[#f6f6f6c4] hover:text-[#F6F6F6] border-b border-[#121212] hover:border-[#f6f6f654] text-2xl sm:text-3xl sm:pb-5 font-[300] leading-[160%] tracking-[0.7px] transition-all duration-500" onClick={() => setNavbarOpen((prev) => !prev)}
+            className="overflow-hidden ml-10 w-[70%] sm:w-[80%] text-[#f6f6f6c4] hover:text-[#F6F6F6] border-b border-[#121212] hover:border-[#f6f6f654] text-xl sm:text-3xl sm:pb-5 font-[300] leading-[120%] tracking-[0.7px] transition-all duration-500" onClick={() => setNavbarOpen((prev) => !prev)}
         >
             {title}
         </motion.div>
